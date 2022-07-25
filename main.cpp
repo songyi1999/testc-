2297e289c5f221460999fd87f933f8186df29405
songyi <songyi1999@users.noreply.github.com>
2022-07-25 04:20:28 +0000

    save

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-
+#include <time.h>
+#include <ctime>
 using namespace std;
 
 int main(){
-
-    std::cout << "Hello World!" << std::endl;
+    struct timespec time2 ;
+    clock_gettime( CLOCK_REALTIME, &time2 );
+    //获取微秒时间
+             cout  << "sec" << time2.tv_sec << endl;
+             cout  << "nsec" << time2.tv_nsec << endl;
+                cout  << "sec*1000000:" << time2.tv_sec*1000000 << endl;
 
 }
